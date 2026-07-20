1c7f628b6041ea1276ed0df7f89a782ef17546f8
ferrles124 <mehmetemredursun9@gmail.com>
2026-07-21 00:09:48 +0300

    Add VakitIkonlari object with icon definitions

---
+++ app/src/main/java/com/mehmet/ezanvakti/ui/Vakit ikonları.kt
@@ -0,0 +1,13 @@
+package com.mehmet.ezanvakti.ui
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+
+object VakitIkonlari {
+    val imsak = Icons.Default.Brightness3
+    val gunes = Icons.Default.WbSunny
+    val ogle = Icons.Default.Circle
+    val ikindi = Icons.Default.WbTwilight
+    val aksam = Icons.Default.NightsStay
+    val yatsi = Icons.Default.DarkMode
+}
