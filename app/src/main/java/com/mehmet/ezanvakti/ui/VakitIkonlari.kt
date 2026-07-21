aba3c8be6007652385e042849ce26e3099c2dd9a
ferrles124 <mehmetemredursun9@gmail.com>
2026-07-21 20:53:32 +0300

    Create VakitIkonlari.kt

---
+++ app/src/main/java/com/mehmet/ezanvakti/ui/VakitIkonlari.kt
@@ -0,0 +1,18 @@
+package com.mehmet.ezanvakti.ui
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.mehmet.ezanvakti.R
+
+object VakitIkonlari {
+    val imsak = R.drawable.ic_imsak
+    val gunes = R.drawable.ic_gunes
+    val ogle = R.drawable.ic_ogle
+    val ikindi = R.drawable.ic_ikindi
+    val aksam = R.drawable.ic_aksam
+    val yatsi = R.drawable.ic_yatsi
+    val hourglass = R.drawable.ic_hourglass
+    val mosque = R.drawable.ic_mosque
+    val notification = R.drawable.ic_notification
+    val current = R.drawable.ic_current
+}
