654278476775928c36682b5dff0ac431ff7437fb
ferrles124 <mehmetemredursun9@gmail.com>
2026-07-21 16:01:24 +0300

    Replace icon imports with vector resources

--- app/src/main/java/com/mehmet/ezanvakti/ui/Vakit ikonları.kt
+++ app/src/main/java/com/mehmet/ezanvakti/ui/Vakit ikonları.kt
@@ -1,13 +1,18 @@
 package com.mehmet.ezanvakti.ui
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.mehmet.ezanvakti.R
 
 object VakitIkonlari {
-    val imsak = Icons.Default.Brightness3
-    val gunes = Icons.Default.WbSunny
-    val ogle = Icons.Default.Circle
-    val ikindi = Icons.Default.WbTwilight
-    val aksam = Icons.Default.NightsStay
-    val yatsi = Icons.Default.DarkMode
+    val imsak = ImageVector.vectorResource(id = R.drawable.ic_imsak)
+    val gunes = ImageVector.vectorResource(id = R.drawable.ic_gunes)
+    val ogle = ImageVector.vectorResource(id = R.drawable.ic_ogle)
+    val ikindi = ImageVector.vectorResource(id = R.drawable.ic_ikindi)
+    val aksam = ImageVector.vectorResource(id = R.drawable.ic_aksam)
+    val yatsi = ImageVector.vectorResource(id = R.drawable.ic_yatsi)
+    val hourglass = ImageVector.vectorResource(id = R.drawable.ic_hourglass)
+    val mosque = ImageVector.vectorResource(id = R.drawable.ic_mosque)
+    val notification = ImageVector.vectorResource(id = R.drawable.ic_notification)
+    val current = ImageVector.vectorResource(id = R.drawable.ic_current)
 }
